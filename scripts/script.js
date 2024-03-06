d4a75b8ada2e2accb105b0b50bd16bf2665848af
smaldonado1326 <157517882+smaldonado1326@users.noreply.github.com>
2024-03-06 17:14:45 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
-$(document).ready(function() {
-  var myNavbar = document.getElementById('navbarNav');
-  var bsNavbar = new bootstrap.Collapse(myNavbar, {
-    toggle: false
+$(document).ready(function(){
+  // Toggle navbar visibility on scroll
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+       
+        $('.navbar').slideUp();
+    } else {
+    
+       $('.navbar').slideDown();
+    }
+    lastScrollTop = st;
+  });
+
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+
+  $('.navbar-toggler').on('click', function() {
+    $('.navbar-collapse').collapse('toggle');
   });
 });
